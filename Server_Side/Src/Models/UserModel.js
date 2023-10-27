1c2f82be50c4de3c2ec049123eef88dae07e29ce
suhara beevi <sairasuhara12345@gmail.com>
2023-10-27 21:22:48 +0530

    mongodb connection error fixed

--- Server_Side/Src/Models/UserModel.js
+++ Server_Side/Src/Models/UserModel.js
@@ -4,12 +4,11 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   phone: { type: String, required: false },
   place: { type: String, required: false, trim: true },
-  address: { type: String, required: false, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true, trim: true },
   status: { type: Boolean, required: true, default: true },
 });
 
-const User = mongoose.model("User", UserSchema);
+const USER = mongoose.model("User", UserSchema);
 
-module.exports = User;
+module.exports = USER;
